e0977e0e4c10884ff00082dc6329672f8a38b309
Pavel Kuzmuk <pasha.kuzmuk@gmail.com>
2020-05-03 14:33:09 +0300

    Refactor test flow, fully remove alternative testing with interactive rebase

--- git-fast-reword_test.go
+++ git-fast-reword_test.go
@@ -1,183 +1,62 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 	"reflect"
 	"strconv"
 	"testing"
-
-	git "github.com/libgit2/git2go/v28"
 )
 
 const (
-	testDir        = "test_repos"
-	interactiveDir = testDir + "/interactive"
-	fastDir        = testDir + "/fast"
+	testDir = "test_repo/"
 
 	newCommitMessage   = "new_commit\n"
 	initRepoScriptsDir = "init_repo"
-	branchName         = "Fast_Reword_Branch"
-	testCases          = 4
-	outputHashSize     = 40
+	outputHashLen      = 40
 )
 
-// Works only for chains
-func interactiveRebaseReword(repoRoot string, params []rewordParam) error {
-	g, err := buildCommitGraph(repoRoot)
-	if err != nil {
-		return err
-	}
-	rebaseOnRoot := false
-	commits := make([]*commit, 0)
-	for _, v := range params {
-		c := g.GetCommit(v.hash)
-		if c == nil {
-			return fmt.Errorf("commit not found")
-		}
-		if len(c.parents) > 1 {
-			return fmt.Errorf("branching / merges not supported")
-		}
-		if len(c.parents) == 0 {
-			rebaseOnRoot = true
-		}
-		commits = append(commits, c)
-	}
-	cnf := make(map[string]bool)
-	for _, v := range commits {
-		cnf[v.id] = true
-	}
-	repo, err := git.OpenRepository(repoRoot)
-	if err != nil {
-		return err
-	}
-	hr, err := repo.Head()
-	if err != nil {
-		return err
-	}
-	topcommit, err := repo.LookupCommit(hr.Branch().Target())
-	if err != nil {
-		return err
-	}
-	tc := g.GetCommit(topcommit.Id().String())
-	var last string
-	commitOrder := make([]string, 0)
-	for {
-		commitOrder = append(commitOrder, tc.id)
-		if cnf[tc.id] {
-			delete(cnf, tc.id)
-		}
-		if len(cnf) == 0 {
-			last = tc.id
-			break
-		}
-		if len(tc.parents) != 1 {
-			return fmt.Errorf("branching / merges not supported or HEAD is not on the same branch as one of commits")
-		}
-		tc = tc.parents[0]
-	}
-	c := g.GetCommit(last)
-	if !rebaseOnRoot {
-		c = c.parents[0]
-	}
-	wd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	what := c.id
-	if rebaseOnRoot {
-		what = "--root"
-	}
-
-	newMessage := make(map[string]string)
-	for _, v := range params {
-		newMessage[v.hash] = v.message
-	}
-
-	var rebaseConfig string
-	mm := make([]string, 0)
-	for idx := len(commitOrder) - 1; idx >= 0; idx-- {
-		if newmsg, ok := newMessage[commitOrder[idx]]; ok {
-			rebaseConfig += fmt.Sprintf("e %s\n", commitOrder[idx])
-			mm = append(mm, newmsg)
-		} else {
-			rebaseConfig += fmt.Sprintf("p %s\n", commitOrder[idx])
-
-		}
-	}
-	cmd := fmt.Sprintf("cd $(pwd)/%s && GIT_SEQUENCE_EDITOR=\"%s/configure_interactive_rebase.sh '%s'\" git rebase -i %s", repoRoot, wd, rebaseConfig, what)
-	if err = exec.Command("/bin/sh", "-c", cmd).Run(); err != nil {
-		return err
-	}
-
-	for _, v := range mm {
-		cmd = fmt.Sprintf("cd $(pwd)/%s && git commit --amend -m \"%s\" && git rebase --continue", repoRoot, v)
-		if err = exec.Command("/bin/sh", "-c", cmd).Run(); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 func TestMain(t *testing.T) {
-	for i := 1; i <= testCases; i++ {
+	ird, err := os.Open(initRepoScriptsDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	names, err := ird.Readdirnames(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i := 1; i <= len(names); i++ {
 		t.Run("test "+strconv.Itoa(i), func(t *testing.T) {
-			hashes1, err := exec.Command("./"+initRepoScriptsDir+"/"+strconv.Itoa(i)+".sh", interactiveDir).Output()
+			hashes, err := exec.Command("./"+initRepoScriptsDir+"/"+strconv.Itoa(i)+".sh", testDir).Output()
 			if err != nil {
 				t.Error(err)
 			}
-			hashes2, err := exec.Command("./"+initRepoScriptsDir+"/"+strconv.Itoa(i)+".sh", fastDir).Output()
-			if err != nil {
-				t.Error(err)
-			}
-
-			params1 := make([]rewordParam, 0)
-			for i := 0; i < len(hashes1)/outputHashSize; i++ {
-				params1 = append(params1,
-					rewordParam{string(hashes1[i*outputHashSize : (i+1)*outputHashSize]), newCommitMessage},
-				)
-			}
-			params2 := make([]rewordParam, 0)
-			for i := 0; i < len(hashes2)/outputHashSize; i++ {
-				params2 = append(params2,
-					rewordParam{string(hashes2[i*outputHashSize : (i+1)*outputHashSize]), newCommitMessage},
+			params := make([]rewordParam, 0)
+			for i := 0; i < len(hashes)/outputHashLen; i++ {
+				params = append(params,
+					rewordParam{string(hashes[i*outputHashLen : (i+1)*outputHashLen]), newCommitMessage},
 				)
 			}
 
-			g, err := buildCommitGraph(interactiveDir)
+			g, err := buildCommitGraph(testDir)
 			if err != nil {
 				t.Error(err)
 			}
-			g.Reword(params1)
+			g.Reword(params)
 
-			/*if err := interactiveRebaseReword(interactiveDir, params1); err != nil {
-				t.Error(err)
-			}*/
-			if err := fastReword(fastDir, params2); err != nil {
+			if err := fastReword(testDir, params); err != nil {
 				t.Error(err)
 			}
 
-			compareRepos(t, interactiveDir, fastDir, g)
+			g1, err := buildCommitGraph(testDir)
+			if err != nil {
+				t.Error(err)
+			}
+			if !g1.Equal(g) {
+				t.Errorf("fast rebased graph is wrong")
+			}
 		})
 		os.RemoveAll(testDir)
-	}
-}
-
-func compareRepos(t *testing.T, repo1, repo2 string, g *repoGraph) {
-	g1, err := buildCommitGraph(repo1)
-	if err != nil {
-		t.Error(err)
-	}
-	g2, err := buildCommitGraph(repo2)
-	if err != nil {
-		t.Error(err)
-	}
-	if !g1.Equal(g) && false {
-		t.Errorf("interactive rebased graph is wrong")
-	}
-	if !g2.Equal(g) {
-		t.Errorf("fast reworded graph is wrong")
 	}
 }
 
